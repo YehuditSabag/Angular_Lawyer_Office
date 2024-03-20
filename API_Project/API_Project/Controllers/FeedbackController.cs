2a582b16ea1d3d5deb58790110c313f036ffaf79
YehuditSabag <124213041+YehuditSabag@users.noreply.github.com>
2024-03-20 20:08:34 +0200

    Update FeedbackController.cs

--- API_Project/API_Project/Controllers/FeedbackController.cs
+++ API_Project/API_Project/Controllers/FeedbackController.cs
@@ -18,45 +18,11 @@ namespace API_Project.Controllers
         {
             return Ok(DB.listFeedback);
         }
-
-        //[HttpGet("{id}")]
-        //public IHttpActionResult Get(int id)
-        //{
-
-        //    Feedback feedback= DB.listFeedback.Find(c => c.id == id);
-        //    if (feedback == null)
-        //        return BadRequest("Id not found");
-        //    return Ok(feedback);
-        //}
-
         [HttpPost]
         public IHttpActionResult Post([FromBody]Feedback feedback)
         {
             DB.listFeedback.Add(feedback);
             return Ok(DB.listFeedback);
-
         }
-
-        //[HttpPut("{id}")]
-        //public IHttpActionResult Put([FromBody] Feedback feedback)
-        //{
-        //    Feedback f = DB.listFeedback.Find(c => c.id == feedback.id);
-        //    if (f == null)
-        //        return BadRequest("Id not found");
-        //    f.id = feedback.id;
-        //    f.name = feedback.name;
-        //    f.description = feedback.description;
-        //    return Ok(DB.listFeedback);
-        //}
-
-        //[HttpDelete("{id}")]
-        //public IHttpActionResult Delete([FromBody] int id)
-        //{
-        //    Feedback feedback = DB.listFeedback.Find(c => c.id == id);
-        //    if (feedback == null)
-        //        return BadRequest("Id not found");
-        //    DB.listFeedback.Remove(feedback);
-        //    return Ok(DB.listFeedback);
-        //}
     }
 }
